a99ae90695eee632adf44bc4aaa1d3bfbb238961
jok1 <jok1@>
2024-01-28 17:29:50 +0800

    update

---
+++ apps/game_bingo/src/bingo_game_logic.erl
@@ -0,0 +1,21 @@
+-module(bingo_game_logic).
+
+-define(GAME_ID, bingo).
+-export([speaker_reads_number/0]).
+
+%% @doc
+speaker_reads_number() ->
+    lists:foldl(fun(SequenceId,{SpeakNumbers,LastNumberList})->
+            L = length(LastNumberList),
+        NumberIndex = rng_generator:random(?GAME_ID, L),
+                Number = lists:nth(NumberIndex,LastNumberList),
+
+        NewSpeakNumbers = [{SequenceId,Number}|SpeakNumbers],
+        NewLastNumberList = LastNumberList -- [Number],
+        {NewSpeakNumbers,NewLastNumberList}
+                end,{[],number_list()},lists:seq(1,35)).
+
+number_list()->
+    lists:seq(1,75).
+
+
